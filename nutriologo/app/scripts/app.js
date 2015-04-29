8140ad245a9b5dd22c14ec19c36cc3a78d6edb54
monterito <monteromalj@gmail.com>
2015-04-29 08:54:37 -0500

    mike

---
+++ nutriologo/app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nutriologoApp
+ * @description
+ * # nutriologoApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nutriologoApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
---
+++ nutriologo/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nutriologoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nutriologoApp
+ */
+angular.module('nutriologoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
